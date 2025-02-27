70b3840b35038b480c84c466743b091755a62355
IRASUBIZA-SALY-NELSON <irasubizasalynelson@gmail.com>
2025-02-27 19:03:59 +0200

    register and login

--- config.js
+++ config.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://irasubizasalynelson:agrisense@agrisense.gjhd0.mongodb.net/?retryWrites=true&w=majority&appName=AGRISENSE', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect('mongodb+srv://irasubizasalynelson:agrisense@agrisense.gjhd0.mongodb.net/?retryWrites=true&w=majority&appName=AGRISENSE');
         console.log('connected to database successfully!');
     } catch (error) {
         console.error('MongoDB connection error:', error);
